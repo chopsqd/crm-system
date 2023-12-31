5ec97ffb8fc7cd24d907da84096d74dff8d21c2f
Michael Chop <007micle007@gmail.com>
2023-12-31 18:46:02 +0300

    Implemented the basic functionality of the position controller

--- server/controllers/position.js
+++ server/controllers/position.js
@@ -1,15 +1,55 @@
-module.exports.getByCategoryId = function (req, res) {
-    res.send('overview')
+const Position = require('../models/Position')
+const errorHandler = require("../utils/errorHandler");
+
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.id,
+            user: req.user.id
+        })
+
+        return res.status(200).json(positions)
+    } catch(error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.create = function (req, res) {
-    res.send('analytics')
+module.exports.create = async function (req, res) {
+    try {
+        const {name, cost, category} = req.body
+
+        const position = new Position({
+            name, cost, category, user: req.user.id
+        })
+
+        await position.save()
+
+        return res.status(201).json(position)
+    } catch(error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.send('overview')
+module.exports.remove = async function (req, res) {
+    try {
+        await Position.remove({_id: req.params.id})
+
+        return res.status(200).json({message: 'Позиция была удалена'})
+    } catch(error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.update = function (req, res) {
-    res.send('analytics')
+module.exports.update = async function (req, res) {
+    try {
+        const position = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+
+        return res.status(200).json(position)
+    } catch(error) {
+        errorHandler(res, error)
+    }
 }

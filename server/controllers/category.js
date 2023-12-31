d4ae068c43cfcd545aa5d2720fc35a78a177dec5
Michael Chop <007micle007@gmail.com>
2023-12-31 18:56:18 +0300

    Added the ability to get and delete categories

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,19 +1,50 @@
-module.exports.getAll = function (req, res) {
-    res.send('overview')
+const Category = require('../models/Category')
+const Position = require('../models/Position')
+const errorHandler = require("../utils/errorHandler");
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id})
+
+        return res.status(200).json(categories)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.getById = function (req, res) {
-    res.send('analytics')
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id)
+
+        return res.status(200).json(category)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.send('overview')
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+
+        return res.status(200).json({message: 'Категория была удалена'})
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.create = function (req, res) {
-    res.send('analytics')
+module.exports.create = async function (req, res) {
+    try {
+
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.update = function (req, res) {
-    res.send('analytics')
+module.exports.update = async function (req, res) {
+    try {
+
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }

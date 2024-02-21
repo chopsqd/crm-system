dfc98dd237e6f8138b2174a274ee82ebe24fc35e
Michael Chop <007micle007@gmail.com>
2024-02-22 00:05:37 +0300

    Added elements loading and pagination

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,5 +1,10 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {IMaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrdersService} from "../shared/services/orders.service";
+import {Subscription} from "rxjs";
+import {IOrder} from "../shared/interfaces";
+
+const STEP = 2
 
 @Component({
   selector: 'app-history-page',
@@ -11,16 +16,51 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('tooltip') tooltipRef: ElementRef
   tooltip: IMaterialInstance
   isFilterVisible = false
+  orders: IOrder[] = []
+  oSub: Subscription
 
-  constructor() {}
+  offset = 0
+  limit = STEP
 
-  ngOnInit() {}
+  loading = false
+  reloading = false
+  noMoreOrders = false
+
+  constructor(private ordersService: OrdersService) {}
+
+  ngOnInit() {
+    this.reloading = true
+    this.fetch()
+  }
 
   ngOnDestroy() {
     this.tooltip.destroy()
+    this.oSub.unsubscribe()
   }
 
   ngAfterViewInit() {
     this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+  }
+
+  private fetch() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    }
+
+    this.oSub = this.ordersService.fetch(params).subscribe(
+      orders => {
+        this.orders = this.orders.concat(orders)
+        this.noMoreOrders = orders.length < STEP
+        this.loading = false
+        this.reloading = false
+      }
+    )
+  }
+
+  loadMore() {
+    this.offset += STEP
+    this.loading = true
+    this.fetch()
   }
 }
--- client/src/app/shared/services/orders.service.ts
+++ client/src/app/shared/services/orders.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {IOrder} from "../interfaces";
 import {Observable} from "rxjs";
@@ -6,6 +6,14 @@ import {Observable} from "rxjs";
 @Injectable({providedIn: 'root'})
 export class OrdersService {
   constructor(private  http: HttpClient) {}
+
+  fetch(params: any = {}): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>('/api/order', {
+      params: new HttpParams({
+        fromObject: params
+      })
+    })
+  }
 
   create(order: IOrder): Observable<IOrder> {
     return this.http.post<IOrder>('/api/order', order)
